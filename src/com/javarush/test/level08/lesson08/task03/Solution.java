3ef5a32558cfd122db208a01672f3cf9c708ebfc
Docnt <marcin.mal92@gmail.com>
2017-08-10 15:28:02 +0200

    JavaRush Solutions

--- src/com/javarush/test/level08/lesson08/task03/Solution.java
+++ src/com/javarush/test/level08/lesson08/task03/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.test.level08.lesson08.task03;
 
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 
 /* Identical first and last names
 Create a dictionary (Map<String, String>) and add ten entries according to the model «last name» - «first name».
@@ -12,19 +12,38 @@ public class Solution
 {
     public static HashMap<String, String> createMap()
     {
-        //add your code here
+        HashMap<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < 10; i++)
+            map.put("L" + i, "M" + i);
+        return map;
 
     }
 
     public static int getCountTheSameFirstName(HashMap<String, String> map, String name)
     {
-        //add your code here
-
+        int i = 0;
+        for (Map.Entry<String, String> mapa : map.entrySet())
+        {
+            if (name.equals(mapa.getValue())) i++;
+        }
+        return i;
     }
 
     public static int getCountTheSameLastName(HashMap<String, String> map, String familiya)
     {
-        //add your code here
+        int i = 0;
+        for (Map.Entry<String, String> mapa : map.entrySet())
+        {
+            if (familiya.equals(mapa.getKey())) i++;
+        }
+        return i;
 
+    }
+
+    public static void main(String[] args)
+    {
+        HashMap<String, String> nazwiska = createMap();
+        System.out.println(getCountTheSameFirstName(nazwiska, "M2"));
+        System.out.println(getCountTheSameLastName(nazwiska, "L2"));
     }
 }
